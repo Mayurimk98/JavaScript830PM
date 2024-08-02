6cdad95419156f79cc311d698bf4936569fb4b05
Mayurimk98 <mayurimkatwe@gmail.com>
2024-08-02 21:19:59 +0530

    script1.js

---
+++ script1.js
@@ -0,0 +1,83 @@
+//Javascript
+
+//print
+
+console.log("Mayuri")
+console.log(10)
+
+console.log(10 + 10)//20
+
+let firstName = "Sonu"
+//keyword variableName =value
+console.log(firstName)
+// console.log(firstname)
+
+const lastName = "pati"
+console.log(lastName)//pati
+
+//update
+let a = 10
+a = 30  //update
+console.log(a)//30
+
+
+// const b = 50
+// console.log(b)//50
+// b = 100
+// console.log(b)//error
+
+
+//opeartors
+//1. arithmatic opearator
+//2. comparison operator
+//3. logical opeartor
+
+
+//1. arithmatic opeartor
+//addition => +
+//sub => -
+//multiplication => *
+//division => /
+//modulus => %
+
+let x = 10
+let y = 5
+console.log(x + y) //15
+console.log(x - y) //5
+console.log(x * y)//50
+console.log(x / y)//2
+console.log(x % y)//0
+
+
+let s = 30
+let v = 5
+console.log(s + v)
+console.log(s - v)
+console.log(s * v)
+console.log(s / v)
+console.log(s % v)
+
+//10 pair => 50 line
+
+//DRY => donot repeat yourself
+//time , effort, memory
+
+//functions
+
+function operation(a,b){
+    console.log(a+b)
+    console.log(a-b)
+    console.log(a*b)
+    console.log(a/b)
+    console.log(a%b)
+}
+operation(20,2)
+
+//100,20
+operation(100,20)
+
+
+
+
+
+
