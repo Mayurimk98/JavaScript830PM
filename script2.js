51c14cc611596d5ae9d888ec1d951e662dd5dd6c
Mayurimk98 <mayurimkatwe@gmail.com>
2024-08-05 22:51:39 +0530

    script2

---
+++ script2.js
@@ -0,0 +1,134 @@
+// // //function
+
+
+// // let a=20
+// // let b=10
+// // console.log(a+b)
+// // console.log(a-b)
+// // console.log(a*b)
+// // console.log(a/b)
+// // console.log(a%b)
+
+// // let s=100
+// // let v=50
+
+// // //syntax
+
+// // function calculator(a,b){
+// //     console.log(a+b)
+// //     console.log(a-b)
+// //     console.log(a*b)
+// //     console.log(a/b)
+// //     console.log(a%b)  
+// // }
+// // calculator(10,2)
+
+// //function => keyword
+// //calculator=> function name
+// //()=> paranthesis
+// //(a,b)=> parameter
+// //{}=> block
+// //statemenets
+// //calculator(10,2) => function calling => (10,2)=> arguments
+
+// //======================================================================
+
+// //3 types
+
+// //1. function without para and without return type
+// function sub() {
+//     console.log(55 - 5)
+// }
+// sub()
+// sub()
+// sub()
+// sub()
+
+// //output never changes
+// //output cant resuse
+
+// //2. function with para and without return type
+
+// // function sub1(x, y) {
+// //     console.log(x - y)
+// // }
+// // sub1(30, 2)//28
+// // sub1(50, 12)//38
+// // sub1(100, 10)//90
+
+// //output is changing
+// //output cant resuse
+
+
+// //100 rs show => 
+// //100 rs give
+
+// //3. function with para and with return type
+
+// // function sub2(a, b) {
+// //     return a - b
+// //     //return 300-50
+// // }
+// // let a1 = sub2(300, 50)
+// // console.log(a1)
+// // console.log(a1+20)
+// // console.log(a1-20)
+
+// //=======================================================================
+
+// let a = 20 
+// //20.12 ,-20, 00.2345,100
+// console.log(typeof a)
+
+// let b="mayuri"
+
+// let b1='shree'
+
+// let c1=`ganesh`
+// //string
+
+// console.log(typeof b)
+// console.log(typeof b1)
+// console.log(typeof c1)
+
+// //boolean
+// //true false
+
+// let s=true
+// console.log(typeof s)
+// let v=false
+// console.log(typeof v)
+
+
+// let r="true"
+// console.log(typeof r)
+
+// let g='34567'
+// console.log(typeof g)
+
+//=====================================
+
+// let q1=100
+// q1=567
+// console.log(q1)//567
+// console.log(q1)//567
+
+// // let q1=300
+// // console.log(q1)//error
+
+// const s1=50
+// s1=78
+// console.log(s1)//error
+
+
+const t=22
+//t=30
+//console.log (t)//error
+//=====================================
+let y1=100
+y1=50
+console.log(y1)//
+
+
+
+
