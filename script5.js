84cd92e8deca0d95e90958c3b1a05b63a4288430
Mayurimk98 <mayurimkatwe@gmail.com>
2024-08-08 20:58:59 +0530

    script5

---
+++ script5.js
@@ -0,0 +1,156 @@
+
+//multiple condition
+//if if ==> 
+
+//if else if => 
+
+//single condition
+//if else
+
+//single condition
+
+// let a = 10
+// let b = 70
+// // if (a > b) {
+// //     console.log('a is greater')
+// // }
+// // else {
+// //     console.log('b is greater')
+// // }
+
+// //ternary opeartor
+
+// //syntax
+// //condition(expression) ? true statemenet : false statemenet
+
+// //a>b ?  console.log('a is greater') :  console.log('b is greater')
+
+// let result = a > b ? "a is greater" : "b is greater"
+// console.log(result)
+
+//====================================================================================
+
+//multiple condition
+
+//switch case 
+
+//1. switch case without break
+// let city = "Jaipur"
+
+// switch (city) {
+//     case "Pune":
+//         console.log("MH")
+//     case "Jaipur":
+//         console.log('RJ')
+//     case "Indore":
+//         console.log("MP")
+// }
+
+
+// let city = "Jaipur"
+
+// switch (city) {
+//     case "Pune":
+//         console.log("MH")
+//         break
+//     case "Jaipur":
+//         console.log('RJ')
+//         break
+//     case "Indore":
+//         console.log("MP")
+//         break
+// }
+
+// let city = "pune"
+
+// switch (city) {
+//     case "Pune":
+//         console.log("MH")
+//         break
+//     case "Jaipur":
+//         console.log('RJ')
+//         break
+//     case "Indore":
+//         console.log("MP")
+//         break
+//     default:
+//         console.log("Invalid input")
+// }
+
+//=======================================================
+
+//traffic light
+
+//red => stop
+//yellow => get ready
+//green => go
+//other than above three => invalid traffic light
+
+// let trafficLight ="green"
+// switch (trafficLight){
+//     case "red":
+//         console.log("Stop")
+//         break
+//     case "yellow":
+//         console.log('get ready')
+//         break
+//     case "green":
+//         console.log('go')
+//         break
+//     default:
+//         console.log('Invalid traffic light')
+// }
+
+// let trafficLight = "green"
+// let action
+// switch (trafficLight) {
+//     case "red":
+//         action = "Stop"
+//         break
+//     case "yellow":
+//         action = 'get ready'
+//         break
+//     case "green":
+//         action = 'go'
+//         break
+//     default:
+//         action = 'Invalid traffic light'
+// }
+// console.log(action)
+
+//==============================================================
+
+// let fruit ="banana"
+// switch(fruit){
+//     case "apple":
+//     case "banana":
+//     case "mango":
+//         console.log('this is common fruit')
+//          break
+//     case "durian":
+//         console.log('this is rare friut')
+//         break
+//     default:
+//         console.log('invalid')
+
+// }
+
+//==============================================
+
+let fruit = "red"
+switch (fruit) {
+    case "apple":
+    case "banana":
+    case "mango":
+        console.log('this is common fruit')
+        break
+    case "XYZ":
+    default:
+        console.log('invalid')
+
+}
+
+
+
+
+
