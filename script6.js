9b5a2f0f9b61643d9bf382909ce2c1368f9565a8
Mayurimk98 <mayurimkatwe@gmail.com>
2024-08-09 20:56:19 +0530

    script6

---
+++ script6.js
@@ -0,0 +1,92 @@
+//3 numbers
+
+let x1 = 20
+let x2 = 700
+let x3 = 110
+
+//if else if
+
+// if (x1 > x2 && x1 > x3) {
+//     console.log('X1 is greater')
+// }
+// else if (x2 > x1 && x2 > x3) {
+//     console.log('X2 is greater')
+// }
+// else {
+//     console.log('X3 is greater')
+// }
+
+//================================================
+//switch case
+// let biggest = true
+// switch (biggest) {
+//     case x1 > x2 && x1 > x3:
+//         console.log('x1 is greater')
+//         break
+//     case x2 > x1 && x2 > x3:
+//         console.log('x2 is greater')
+//         break
+//     default:
+//         console.log('x3 is greater')
+
+// }
+
+//=================================================
+
+//loop
+
+//for loop => duration
+//while loop => duration is not known
+//do while => 
+
+//mayuri => 5 function
+
+//function => repetative operation avoid
+//loop => repetative operation based on condition
+
+
+//let s
+//let v
+//5 line
+//5 line 
+//function
+
+//mayuri => 5 print
+//repetation
+
+//for loop
+
+//syntax
+
+// for(initialization;conditionCheck;incre/decre){
+//     //stateemenet
+// }
+
+//1- 5
+
+for (let i = 1; i <= 5; i++) {
+    console.log(i)
+}
+
+
+//10-0
+
+for (let i = 10; i >= 0; i--) {
+    console.log(i)
+}
+
+//12 table
+
+for (let i = 12; i <= 120; i = i + 12) {
+    console.log(i)
+}
+
+//5 table reverese
+
+for (let i = 50; i >= 5; i = i - 5) {
+    console.log(i)
+}
+
+
+
+
