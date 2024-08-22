3e2a70448c261337893a46657ad29c4f5f18bc79
Mayurimk98 <mayurimkatwe@gmail.com>
2024-08-22 21:06:13 +0530

    script12

---
+++ script12.js
@@ -0,0 +1,139 @@
+let student = ["Mayuri", "ram", "sham", "radhe"]
+//index          0         1      2       3
+
+//array
+//typeof => object
+
+console.log(student[2])
+
+//javascript
+
+//object
+//methods() => 1
+//action  => 1
+//return => 2
+
+//property => 2
+
+//mayuri => object => humman
+//methods() => walk(), talk(), study()
+//action  => read, write
+//returnn => good job, good marks
+//propeties => height, weight, birthyesr, age, color
+
+
+//Array => object
+//methods()
+//properties
+
+let fruits = ["mango", "apple", "orange", "grapes", "santra", "dragenfruits"]
+//  index         0        1       2        3
+//  length        1        2      3        4
+
+//array => length => property => number
+//4
+
+console.log(fruits.length)//4
+
+//========================================
+//array last index
+
+console.log(fruits.length - 1)
+
+//arry first index => 0
+
+
+//loop
+
+// for (let i = 0; i < 4; i++) {
+//     console.log(fruits[i])
+// }
+
+//i<4
+// for (let i = 0; i < fruits.length; i++) {
+//     console.log(fruits[i])
+// }
+
+
+// for(let i=fruits.length-1;i>=0;i--){
+//     console.log(fruits[i])
+// }
+
+// let i = 0
+// while (i < fruits.length) {
+//     console.log(fruits[i])
+//     i++
+// }
+
+// let j=fruits.length-1
+// while(j>=0){
+//     //console.log(j)
+//     console.log(fruits[j])
+//     j--
+// }
+
+//===============================================
+
+//methods() => function
+//call => functionname()
+
+//array 
+
+//1. push()
+//action => add the element at last
+//return=> new length of array
+
+// let city = ["nashik", "pune", "mumbai", "yeola", "jalgoan"]
+//nagpur
+
+// let result = city.push('nagpur')
+// console.log(result)//6
+// console.log(city)
+
+//2. unshift()
+//action => add the ele at first
+//return  => new length of array
+
+// let s1 = city.unshift('sangamner')
+// console.log(s1)//6
+// console.log(city)
+
+//3. pop()
+//action => remove elemnt from last
+//resturn  => removed element
+
+// let s2=city.pop()
+// console.log(s2)//jalgoan
+// console.log(city)
+
+
+//4. shift()
+//action => remove ele from first
+//return => removed ele
+
+let city = ["nashik", "pune", "mumbai", "yeola", "jalgoan"]
+
+let s4 = city.shift()
+console.log(s4)//nashik
+console.log(city)
+
+
+//array method and property
+//length => number
+
+//push()=> add ele at last => new length
+//unshift()=> add ele at first => new length
+//pop()=> remove ele from last=> removed ele
+//shift()=> remove elem from first => remove ele
+
+
+
+
+
+
+
+
+
+
+
+
