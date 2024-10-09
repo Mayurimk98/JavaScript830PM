085dc64744851c6063c1a43a53d15f414eba18f6
Mayurimk98 <mayurimkatwe@gmail.com>
2024-10-09 21:00:30 +0530

    domlast

---
+++ DOM8/script.js
@@ -0,0 +1,105 @@
+
+{/* <ul>
+        <li>Mayuri</li>
+        <li>Prashant</li>
+        <li>Vaibhav</li>
+        <li>Jaya</li>
+    </ul> */}
+
+//SingleElement
+// let ele1 = document.querySelector('li')
+// //console.log(ele1) //html element
+
+// //MultiPle element
+// let ele2 = document.querySelectorAll('li')
+// console.log(ele2)//nodelist
+
+//==================================================================
+{/* <h1 id="std" class="one two three" name="nm">Student</h1> */ }
+//tagname
+//id
+//class
+//CF
+
+// let ele3 = document.getElementById('std')
+// console.log(ele3) //htmlElement
+
+// let ele4 = document.getElementsByClassName('two')
+// console.log(ele4)//htmlCollection
+
+// let ele5 = document.getElementsByName('nm')
+// console.log(ele5)//nodeList
+
+// let ele6=document.getElementsByTagName('h1')
+// console.log(ele6)//htmlCollection
+
+//Nodelist and Htmlcollection diff
+
+// let ele2 = document.querySelectorAll('li')
+// console.log(ele2)//nodelist //static
+
+
+// let ele7=document.getElementsByTagName('li')
+// console.log(ele7)//htmlCollection //dynamic
+
+//=======================================================================
+{/* <h1 id="std" class="one two three" name="nm">Student</h1> */ }
+
+//className
+//classLIst
+
+// let ele3 = document.getElementById('std')
+// console.log(ele3) //htmlElement
+
+// console.log(ele3.className) //property
+
+// console.log(ele3.classList)//object
+
+// //add()
+// // ele3.classList.add('four')
+// // console.log(ele3.classList)//object
+
+// // //remove()
+// // ele3.classList.remove('two')
+// // console.log(ele3.classList)
+
+// //toggle()
+
+// ele3.classList.toggle('ten')
+// console.log(ele3.classList)
+
+// ele3.classList.toggle('one')
+// console.log(ele3.classList)
+
+
+//============================================================
+
+{/* <h1 id="std" class="one two three" name="nm">Student</h1> */ }
+
+
+// let ele3 = document.getElementById('std')
+// console.log(ele3) //htmlElement
+
+// console.log(ele3.getAttribute('class'))
+
+// console.log(ele3.getAttribute('id'))
+
+// ele3.setAttribute('para','pra')
+// console.log(ele3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
